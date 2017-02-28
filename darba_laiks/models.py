f2419e92a41300aad9144880658484d773cd892d
Olga <olgascelokova@gmail.com>
2017-02-28 12:26:28 +0200

    Izmainīti datu bāzes laika lauki

--- darba_laiks/models.py
+++ darba_laiks/models.py
@@ -3,12 +3,13 @@ from django.conf import settings
 from django.db import models
 from django.core.urlresolvers import reverse
 
+
 # Create your models here.
 class Darba_laiks(models.Model):
     lietotajs=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     datums = models.DateField()
-    no = models.FloatField(default=9.00)
-    lidz=models.FloatField(default=18.00)
+    no = models.TimeField(default='9:00')
+    lidz=models.TimeField(default='18:00')
     ir_mainits = models.BooleanField(default=False)
 
     def __str__(self):
@@ -42,8 +43,8 @@ class Atstrada(models.Model):
     lietotajs = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     darba_laiks= models.ForeignKey(Darba_laiks)
     iemesls= models.ForeignKey(Iemesls)
-    no = models.FloatField()
-    lidz = models.FloatField()
+    no = models.TimeField()
+    lidz = models.TimeField()
     datums = models.DateField()
 
     def __str__(self):
--- dl_uzskaite/settings.py
+++ dl_uzskaite/settings.py
@@ -108,9 +108,11 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
+TIME_INPUT_FORMATS = ['%H:%M',]
+
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
