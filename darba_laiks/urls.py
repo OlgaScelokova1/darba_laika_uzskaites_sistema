801e425711c4f6e345943076169f32abf4b82018
Olga <olgascelokova@gmail.com>
2017-03-01 19:15:45 +0200

    pievienota izrakstīšanas funkcija

--- darba_laiks/urls.py
+++ darba_laiks/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 
+app_name='darba_laiks'
+
 
 urlpatterns = [
     url(r'^$', views.darba_laiks, name='darba_laiks'),
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
 
 ]
--- darba_laiks/views.py
+++ darba_laiks/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.views.generic import View
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
+from django.views.generic import FormView, RedirectView
+
 
 # Create your views here.
 def darba_laiks(request):
     return render(request, "index.html")
+
+class LogoutView(RedirectView):
+    """
+    Provides users the ability to logout
+    """
+    url = '/darba_laiks/'
+
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
