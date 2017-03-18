b8855aae8c3fd2d0c20304b475cd80418f359edb
Laura <laura.strazdina@testdevlab.com>
2017-03-18 22:51:07 +0200

    pievienoju onclick funkcijas un lauku, kur atzīmēt kavējumus

---
+++ static/JavaScript/code.js
@@ -0,0 +1,98 @@
+var Monday = document.getElementById("MondayList").getElementsByClassName("box2");
+
+var Tuesday = document.getElementById("TuesdayList").getElementsByClassName("box2");
+
+var Wednesday = document.getElementById("WednesdayList").getElementsByClassName("box2");
+
+var Thursday = document.getElementById("ThursdayList").getElementsByClassName("box2");
+
+var Friday = document.getElementById("FridayList").getElementsByClassName("box2");
+
+var AllBoxes = document.getElementsByClassName("box2");
+
+var FillBox = document.getElementById("Fill");
+
+var body = document.getElementsByTagName("body");
+
+
+
+console.log(Monday);
+console.log(Tuesday);
+console.log(Wednesday);
+console.log(Thursday);
+console.log(Friday);
+
+
+for (i=0 ; i<9 ; i++) {
+
+    Monday[i].onclick = function () {
+        this.style.backgroundColor = "#d6e2ea";
+        FillBox.style.display = "block";
+        FillBox.style.position = "absolute";
+        FillBox.style.left = "99px";
+        FillBox.style.top = "430px";
+    };
+
+    Tuesday[i].onclick = function () {
+        this.style.backgroundColor = "#d6e2ea";
+        FillBox.style.display = "block";
+        FillBox.style.position = "absolute";
+        FillBox.style.left = "253.5px";
+        FillBox.style.top = "430px";
+    };
+
+    Wednesday[i].onclick = function () {
+        this.style.backgroundColor = "#d6e2ea";
+        FillBox.style.display = "block";
+        FillBox.style.position = "absolute";
+        FillBox.style.left = "403.5px";
+        FillBox.style.top = "430px";
+    };
+
+    Thursday[i].onclick = function () {
+        this.style.backgroundColor = "#d6e2ea";
+        FillBox.style.display = "block";
+        FillBox.style.position = "absolute";
+        FillBox.style.left = "553.5px";
+        FillBox.style.top = "430px";
+    };
+
+    Friday[i].onclick = function () {
+        this.style.backgroundColor = "#d6e2ea";
+        FillBox.style.display = "block";
+        FillBox.style.position = "absolute";
+        FillBox.style.left = "703.5px";
+        FillBox.style.top = "430px";
+    };
+
+//     window.onclick = function (){
+//         if (FillBox.style.display == "block"){
+//             FillBox.style.display = "none";
+//         };
+//         for (j = 0 ; j<45 ; j++){
+//             if (AllBoxes[j].style.backgroundColor === "#d6e2ea"){
+//                 AllBoxes[j].style.backgroundColor = "#a8b6c3";
+//                 alert("test");
+//             };
+//          }
+// }; meģinājumi kko izdomāt
+
+
+}s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
