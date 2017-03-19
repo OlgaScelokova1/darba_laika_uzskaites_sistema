0d2d42c22b6afbfa1012028424af150f66d5c360
Olga <olgascelokova@gmail.com>
2017-03-19 15:55:47 +0200

    lietotājs, kas nav ielogojies, nevar tikt klāt pie informācijas. superuser un staff var veidot jaunus lietotājus

--- darba_laiks/urls.py
+++ darba_laiks/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^$', views.darba_laiks, name='darba_laiks'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^create_user/$', views.UserFormView.as_view(), name='create_user'),
 
 ]
