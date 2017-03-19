a44e466eede5defe1763b011dff75515a0c89233
Laura <laura.strazdina@testdevlab.com>
2017-03-19 16:52:43 +0200

    labojumi ar onClick funkcijām

--- static/JavaScript/code.js
+++ static/JavaScript/code.js
@@ -4,81 +4,65 @@ var Wednesday = document.getElementById("WednesdayList").getElementsByClassName(
 var Thursday = document.getElementById("ThursdayList").getElementsByClassName("box2");
 var Friday = document.getElementById("FridayList").getElementsByClassName("box2");
 
-var AllBoxes = document.getElementsByClassName("box2");
 var FillBox = document.getElementById("Fill");
 
-var body = document.getElementsByTagName("body");
+var reason = document.getElementById("InputReason");
 
+var box = document.getElementsByClassName("box2");
 
 for (i=0 ; i<9 ; i++) {
 
-    Monday[i].onclick = function () {
-        this.style.backgroundColor = "#d6e2ea";
+    Monday[i].onFocus = function () {
         FillBox.style.display = "block";
         FillBox.style.position = "absolute";
         FillBox.style.left = "99px";
         FillBox.style.top = "430px";
+        reason.value = "";
     };
 
     Tuesday[i].onclick = function () {
-        this.style.backgroundColor = "#d6e2ea";
         FillBox.style.display = "block";
         FillBox.style.position = "absolute";
         FillBox.style.left = "253.5px";
         FillBox.style.top = "430px";
+        reason.value = "";
     };
 
     Wednesday[i].onclick = function () {
-        this.style.backgroundColor = "#d6e2ea";
         FillBox.style.display = "block";
         FillBox.style.position = "absolute";
         FillBox.style.left = "403.5px";
         FillBox.style.top = "430px";
+        reason.value = "";
     };
 
     Thursday[i].onclick = function () {
-        this.style.backgroundColor = "#d6e2ea";
         FillBox.style.display = "block";
         FillBox.style.position = "absolute";
         FillBox.style.left = "553.5px";
         FillBox.style.top = "430px";
+        reason.value = "";
     };
 
     Friday[i].onclick = function () {
-        this.style.backgroundColor = "#d6e2ea";
         FillBox.style.display = "block";
         FillBox.style.position = "absolute";
         FillBox.style.left = "703.5px";
         FillBox.style.top = "430px";
+        reason.value = "";
     };
-
-//     window.onclick = function (){
-//         if (FillBox.style.display == "block"){
-//             FillBox.style.display = "none";
-//         };
-//         for (j = 0 ; j<45 ; j++){
-//             if (AllBoxes[j].style.backgroundColor === "#d6e2ea"){
-//                 AllBoxes[j].style.backgroundColor = "#a8b6c3";
-//                 alert("test");
-//             };
-//          }
-// }; meģinājumi kko izdomāt
-
-
 }
 
 
+var save = document.getElementById("Save");
 
+// var sick = document.getElementById("Sick");
+// var vacation = document.getElementById("Vacation");
 
+save.onclick = function (){
+    FillBox.style.display = "none";
 
-
-
-
-
-
-
-
-
+};
 
 
 
