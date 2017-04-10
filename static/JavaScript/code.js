b4ea9ed37b6d2b732f6bc576db95e4f3e1e8faa2
Laura <laura.strazdina@testdevlab.com>
2017-04-10 16:04:57 +0300

    atkal rādās nedēļa

--- static/JavaScript/code.js
+++ static/JavaScript/code.js
@@ -20,6 +20,19 @@ function addDays(date, days) {
     return result;
 }
 
+function getWeek(date) {
+   date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+
+
 var dateMonday = new Date();
 
 while (dateMonday.getDay() !== 1) {
@@ -118,6 +131,9 @@ if(thisMonth<10){
 }
 
 document.getElementById("Month").innerHTML = months[thisMonth-1]; // tiek noteikts mēnesis
+
+document.getElementById("WeekDate").innerHTML = getWeek(dateMonday);
+    
 
 
 
